387f13783ec50880b0b679cf60d63b5a29e38528
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2023-12-04 13:47:58 +0100

    remove: cilLevel enum was not used

--- graphql/schemas/typeDefs.ts
+++ graphql/schemas/typeDefs.ts
@@ -33,9 +33,6 @@ const typeDefs = gql`
     LED
     OTHER
   }
-  enum cilLevel {
-    
-  }
 
   input SortAndPaginate {
     limit: Int!
@@ -70,89 +67,88 @@ const typeDefs = gql`
     photobiologicalSafety: PhotobiologicalSafety
   }
 
-
-#Illuminance
+  #Illuminance
   type Illuminance {
     maintainedAverage: MaintainedAverage
     uniformityRatio: UniformityRatio
   }
 
   type MaintainedAverage {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of maintainedAverage
-}
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of maintainedAverage
+  }
 
-type UniformityRatio {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of uniformityRatio
-}
+  type UniformityRatio {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of uniformityRatio
+  }
 
-#Glare
+  #Glare
 
-type Glare {
-  UGR: UGR
-}
+  type Glare {
+    UGR: UGR
+  }
 
-type UGR {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of UGR
-}
+  type UGR {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of UGR
+  }
 
-# ColorRendering
-type ColorRendering {
-  CRI: CRI
-}
+  # ColorRendering
+  type ColorRendering {
+    CRI: CRI
+  }
 
-type CRI {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of CRI
-}
+  type CRI {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of CRI
+  }
 
-#ColorTemperature 
- type ColorTemperature {
-  CCT: CCT
-  Duv: Duv
-}
+  #ColorTemperature
+  type ColorTemperature {
+    CCT: CCT
+    Duv: Duv
+  }
 
-type CCT {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of CCT
-}
+  type CCT {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of CCT
+  }
 
-type Duv {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of Duv
-}
+  type Duv {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of Duv
+  }
 
-#Flicker
-type Flicker {
-  SVM: SVM
-}
+  #Flicker
+  type Flicker {
+    SVM: SVM
+  }
 
-type SVM {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of SVM
-}
+  type SVM {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of SVM
+  }
 
-#ColorPreference
-type ColorPreference {
-  PVF: PVF
-}
+  #ColorPreference
+  type ColorPreference {
+    PVF: PVF
+  }
 
-type PVF {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of PVF
-}
+  type PVF {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of PVF
+  }
 
-#PhotobiologicalSafety
-type PhotobiologicalSafety {
-  UV: PhotobiologicalSafetyUV
-}
+  #PhotobiologicalSafety
+  type PhotobiologicalSafety {
+    UV: PhotobiologicalSafetyUV
+  }
 
-type PhotobiologicalSafetyUV {
-  value: Float
-  healthStatus: Int # Number from 1-5, indicating the health status of UV
-}
+  type PhotobiologicalSafetyUV {
+    value: Float
+    healthStatus: Int # Number from 1-5, indicating the health status of UV
+  }
 
   type LightingAssetAverageData {
     timestamp: String!
@@ -167,18 +163,18 @@ type PhotobiologicalSafetyUV {
 
   #WorkOrder
   type WorkOrder {
-  _id: ID!
-  workOrderID: String!
-  lightingAssetID: ID!
-  type: WorkOrderType!
-  workOrderStatus: WorkOrderStatus!
-  description: String!
-  comment: String
-  location: Location!
-  dateOfMaintenance: String!
-  excecutionStartDate: String
-  excecutedDate: String
-}
+    _id: ID!
+    workOrderID: String!
+    lightingAssetID: ID!
+    type: WorkOrderType!
+    workOrderStatus: WorkOrderStatus!
+    description: String!
+    comment: String
+    location: Location!
+    dateOfMaintenance: String!
+    excecutionStartDate: String
+    excecutedDate: String
+  }
 
   enum WorkOrderType {
     CM
@@ -247,32 +243,31 @@ type PhotobiologicalSafetyUV {
 
   # Input types and enums for WorkOrder
 
-input AddWorkOrderInput {
-  workOrderID: String!
-  lightingAssetID: ID!
-  type: WorkOrderType!
-  workOrderStatus: WorkOrderStatus!
-  description: String!
-  comment: String
-  location: Location!
-  dateOfMaintenance: String!
-  excecutionStartDate: String
-  excecutedDate: String
-}
+  input AddWorkOrderInput {
+    workOrderID: String!
+    lightingAssetID: ID!
+    type: WorkOrderType!
+    workOrderStatus: WorkOrderStatus!
+    description: String!
+    comment: String
+    location: Location!
+    dateOfMaintenance: String!
+    excecutionStartDate: String
+    excecutedDate: String
+  }
 
-
-input UpdateWorkOrderInput {
-  workOrderID: String
-  lightingAssetID: ID
-  type: WorkOrderType
-  workOrderStatus: WorkOrderStatus
-  description: String
-  comment: String
-  location: Location
-  dateOfMaintenance: String
-  excecutionStartDate: String
-  excecutedDate: String
-}
+  input UpdateWorkOrderInput {
+    workOrderID: String
+    lightingAssetID: ID
+    type: WorkOrderType
+    workOrderStatus: WorkOrderStatus
+    description: String
+    comment: String
+    location: Location
+    dateOfMaintenance: String
+    excecutionStartDate: String
+    excecutedDate: String
+  }
 
   type Query {
     lightingAsset(id: ID!): LightingAsset
