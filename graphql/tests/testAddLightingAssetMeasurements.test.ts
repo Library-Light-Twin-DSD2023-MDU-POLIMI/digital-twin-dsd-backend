c1fe8bcfc1074384c117edd1aa8068b5f4ce94ff
Sankeethan Sivachandran <69573868+sankeets@users.noreply.github.com>
2023-12-08 16:08:37 +0100

    fix: update resolver

--- graphql/tests/testAddLightingAssetMeasurements.test.ts
+++ graphql/tests/testAddLightingAssetMeasurements.test.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import { ILightingAssetMeasurementInput } from "../resolvers/iResolvers/iMutations";
-import resolvers from "../resolvers/resolvers";
+import mongoose from 'mongoose';
+import { ILightingAssetMeasurementInput } from '../resolvers/iResolvers/iMutations';
+import resolvers from '../resolvers/resolvers';
 
 const mockInput: ILightingAssetMeasurementInput[] = [];
 
@@ -17,23 +17,29 @@ for (let i = 0; i < 20; i++) {
 
   // Pushing a new mock data object into the mockInput array
   mockInput.push({
-    assetId: "656494140f014152e06636f3",
+    assetId: '656494140f014152e06636f3',
     timestamp: timestamp.toISOString(),
     // Following are the mock values for different properties
-    illuminance: { maintainedAverage: 100, uniformityRatio: 0.8 },
-    glare: { UGR: 19 },
-    colorRendering: { CRI: 80 },
-    colorTemperature: { CCT: 5000, Duv: 0.003 },
-    flicker: { SVM: 0.5 },
-    colorPreference: { PVF: 1 },
-    photobiologicalSafety: { UV: 0.1 }
+    illuminance: {
+      maintainedAverage: {
+        value: 100,
+      },
+      uniformityRatio: { value: 0.8 },
+    },
+    glare: { UGR: { value: 19 } },
+    colorRendering: { CRI: { value: 80 } },
+    colorTemperature: { CCT: { value: 5000 }, Duv: { value: 0.003 } },
+    flicker: { SVM: { value: 0.5 } },
+    colorPreference: { PVF: { value: 1 } },
+    photobiologicalSafety: { UV: { value: 0.1 } },
   });
 }
 
-describe("addLightingAssetMeasurements Resolver", () => {
+describe('addLightingAssetMeasurements Resolver', () => {
   beforeAll(async () => {
     // Establish a connection to the MongoDB database before running tests
-    const connectionString = "mongodb+srv://application:lol@dsd.iaano1k.mongodb.net/";
+    const connectionString =
+      'mongodb+srv://application:lol@dsd.iaano1k.mongodb.net/';
     await mongoose.connect(connectionString, {});
     // Empty the collection before tests
     await mongoose.model('LightingAssetTimeSeriesData').deleteMany({});
@@ -44,33 +50,49 @@ describe("addLightingAssetMeasurements Resolver", () => {
     await mongoose.connection.close();
   });
 
-  test("should add 20 new lighting asset measurements", async () => {
+  test('should add 20 new lighting asset measurements', async () => {
     // Inserting the mock data into the database
-    const insertionResult = await resolvers.Mutations.LightingAssetTimeSeriesData.addLightingAssetMeasurements(
-      null,
-      { input: mockInput }
-    );
+    const insertionResult =
+      await resolvers.Mutation.addLightingAssetMeasurements(null, {
+        input: mockInput,
+      });
 
     // Assertions to ensure the insertion was successful
     expect(insertionResult).toBeDefined();
     expect(Array.isArray(insertionResult)).toBe(true);
     expect(insertionResult.length).toBe(mockInput.length);
-    
+
     // Verifying each field of every inserted object
     mockInput.forEach((inputObject, index) => {
       const resultObject = insertionResult[index];
       // Expectations to validate the data integrity
-      expect(resultObject.metaData.assetId).toEqual(new mongoose.Types.ObjectId(inputObject.assetId));
+      expect(resultObject.metaData.assetId).toEqual(
+        new mongoose.Types.ObjectId(inputObject.assetId)
+      );
       expect(resultObject.timestamp).toEqual(new Date(inputObject.timestamp));
-      expect(resultObject.illuminance?.maintainedAverage).toBe(inputObject.illuminance?.maintainedAverage);
-      expect(resultObject.illuminance?.uniformityRatio).toBe(inputObject.illuminance?.uniformityRatio);
+      expect(resultObject.illuminance?.maintainedAverage).toBe(
+        inputObject.illuminance?.maintainedAverage
+      );
+      expect(resultObject.illuminance?.uniformityRatio).toBe(
+        inputObject.illuminance?.uniformityRatio
+      );
       expect(resultObject.glare?.UGR).toBe(inputObject.glare?.UGR);
-      expect(resultObject.colorRendering?.CRI).toBe(inputObject.colorRendering?.CRI);
-      expect(resultObject.colorTemperature?.CCT).toBe(inputObject.colorTemperature?.CCT);
-      expect(resultObject.colorTemperature?.Duv).toBe(inputObject.colorTemperature?.Duv);
+      expect(resultObject.colorRendering?.CRI).toBe(
+        inputObject.colorRendering?.CRI
+      );
+      expect(resultObject.colorTemperature?.CCT).toBe(
+        inputObject.colorTemperature?.CCT
+      );
+      expect(resultObject.colorTemperature?.Duv).toBe(
+        inputObject.colorTemperature?.Duv
+      );
       expect(resultObject.flicker?.SVM).toBe(inputObject.flicker?.SVM);
-      expect(resultObject.colorPreference?.PVF).toBe(inputObject.colorPreference?.PVF);
-      expect(resultObject.photobiologicalSafety?.UV).toBe(inputObject.photobiologicalSafety?.UV);
-      });
+      expect(resultObject.colorPreference?.PVF).toBe(
+        inputObject.colorPreference?.PVF
+      );
+      expect(resultObject.photobiologicalSafety?.UV).toBe(
+        inputObject.photobiologicalSafety?.UV
+      );
     });
+  });
 });
