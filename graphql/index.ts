eda5e419f9d95562bacae80d7f563dccc90fb678
Mario Jiménez Redondo <101522353+nezred@users.noreply.github.com>
2023-12-21 18:54:59 +0100

    migrate: sam to serverless

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,37 +1,45 @@
-import { ApolloServer } from 'apollo-server-lambda';
+// Remove unnecessary imports and code for the final version
 import {
   APIGatewayProxyEvent,
+  APIGatewayProxyEventV2,
   APIGatewayProxyResult,
+  APIGatewayProxyResultV2,
   Handler,
 } from 'aws-lambda';
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from '@as-integrations/aws-lambda';
 import mongoose from 'mongoose';
 import resolvers from './resolvers/resolvers';
 import typeDefs from './schemas/typeDefs';
 
 // Construct a schema, using GraphQL schema language
 
+mongoose.connect(
+  'mongodb+srv://application:lol@dsd.iaano1k.mongodb.net/sandbox'
+);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-
-  // By default, the GraphQL Playground interface and GraphQL introspection
-  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-  //
-  // If you'd like to have GraphQL Playground and introspection enabled in production,
-  // install the Playground plugin and set the `introspection` option explicitly to `true`.
   introspection: true,
 });
 
-const baseHandler = server.createHandler();
-// HACK i put the mongoose connection here, ideally this should be handled by the digital-twin-api
+// -------------------- Locally run code -------------------- //
+
+// startStandaloneServer(server).then(({ url }) => console.log(url));
+
+// -------------------- Lambda code -------------------- //
+
 export const lambdaHandler: Handler<
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult
-> = (event, context, callback) => {
-  if (mongoose.connections.length < 1) {
-    mongoose.connect(
-      'mongodb+srv:/application:lol@dsd.iaano1k.mongodb.net/sandbox'
-    );
-  }
-  return baseHandler(event, context, callback);
-};
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2
+> = startServerAndCreateLambdaHandler(
+  server,
+  // We will be using the Proxy V2 handler
+  handlers.createAPIGatewayProxyEventV2RequestHandler()
+);
